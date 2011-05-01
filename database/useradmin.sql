d980b2330ceb90940310d412d273d36af9a05d11
Alexander Bogdanov <syn@li.ru>
2011-05-01 13:44:35 +0400

    Database dump fix. Field width wasn't correct

--- database/useradmin.sql
+++ database/useradmin.sql
@@ -96,7 +96,7 @@ CREATE TABLE `user_tokens` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
   `user_agent` varchar(40) NOT NULL,
-  `token` varchar(32) NOT NULL,
+  `token` varchar(40) NOT NULL, /* sha1 (which is Cookie:salt hashing method) hash - is 40 bytes long*/
   `created` int(10) unsigned NOT NULL,
   `expires` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
